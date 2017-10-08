e3e50bc78fab16a2c69be6f63ac4547684e093a2
kirpichenkov <kirpichenkovspbpu@gmail.com>
2017-10-08 18:50:03 +0300

    extended pom for vertx and coroutines, vertx and kotlin util from kotoed, attempts on working example (for now only works separately)

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,5 +1,91 @@
 package testPackage
 
-fun main (args: Array<String>) {
-    print("Hello, World")
+import builder.FirstVerticle
+import io.vertx.core.AsyncResult
+import io.vertx.core.Vertx
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import util.*
+
+val vertx: Vertx = Vertx.vertx()
+
+//fun main(args: Array<String>) {
+//    var id: String? = null
+//    vertx.deployVerticle(FirstVerticle(), { res ->
+//        if (res.succeeded()) {
+//            id = res.result()
+//            println("Deployment success")
+//        } else {
+//            println("Deployment failure")
+//        }
+//    })
+//    Thread.sleep(2000)
+//    if (id != null)
+//        vertx.undeploy(id, { res ->
+//            if (res.succeeded())
+//                println("Undeploy success")
+//            else {
+//                println("Undeploy failure")
+//            }
+//            vertx.close({ res ->
+//                if (res.succeeded()) {
+//                    println("Close success")
+//                } else {
+//                    println("Close failure")
+//                }
+//            })
+//        })
+//    println("Finishing")
+//}
+
+
+//fun main(args: Array<String>) = runBlocking <Unit> {
+//    val jobs = List(100_000) { // launch a lot of coroutines and list their jobs
+//        launch(Unconfined) {
+//            delay(1000L)
+//            print(".")
+//        }
+//    }
+//    jobs.forEach { it.join() } // wait for all jobs to complete
+//}
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+    doNothingUseful()
+}
+
+
+suspend fun doNothingUseful() {
+    val job = launch(Unconfined) {
+        var id: String? = null
+//        val deployRes = vxa<String> {
+        val deployRes = vxt<AsyncResult<String>> {
+            println("Deploy verticle from coroutine")
+            vertx.deployVerticle(FirstVerticle())
+        }
+        if (deployRes.succeeded()) {
+            id = deployRes.result()
+            println("Deployment success")
+        } else
+            println("Deployment failure")
+        if (id != null){
+            val undeployRes = vxt<AsyncResult<String>> {
+                println("Undeploy verticle from coroutine")
+                vertx.undeploy(id)
+            }
+            if (undeployRes.succeeded())
+                println("Undeploy success")
+            else
+                println("Undeploy failure")
+            val vxcloseRes = vxt<AsyncResult<String>> {
+                println("Close vertx")
+                vertx.close()
+            }
+            if (vxcloseRes.succeeded())
+                println("Close success")
+            else
+                println("Close failure")
+        }
+    }
 }
